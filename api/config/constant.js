9b03eb5d3158988ba73b6ddfb7835fe1b766f332
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-09-30 00:32:56 +0530

    .

--- api/config/constant.js
+++ api/config/constant.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
   ORIGIN: process.env.ORIGIN,
   NODE_ENV: process.env.NODE_ENV,
