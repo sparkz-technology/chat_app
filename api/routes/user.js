b0227e64c59d50ee7cabde4fce1f21f4ace2ba2c
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-09-30 19:53:16 +0530

    try

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,17 +3,11 @@ const router = express.Router();
 
 const { editUserValidator } = require("../validators/user.js");
 const { getAllUsers, editUser, getUser } = require("../controllers/user.js");
-const uploadImage = require("../utils/imageProcess.js");
+// const uploadImage = require("../utils/imageProcess.js");
 const isAuth = require("../middlewares/is-Auth.js");
 
 router.get("/all/:id", isAuth, getAllUsers);
 router.get("/:id", isAuth, getUser);
-router.patch(
-  "/edit/:id",
-  isAuth,
-  uploadImage.single("file"),
-  editUserValidator,
-  editUser
-);
+router.patch("/edit/:id", isAuth, editUserValidator, editUser);
 
 module.exports = router;
