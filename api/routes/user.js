37a92b288e14cb8ca250a7436151e249d8705e99
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-01 16:39:09 +0530

    fileUploaded-removed

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 const { editUserValidator } = require("../validators/user.js");
 const { getAllUsers, editUser, getUser } = require("../controllers/user.js");
-const imageProcess = require("../utils/imageProcess.js");
+// const imageProcess = require("../utils/imageProcess.js");
 const isAuth = require("../middlewares/is-Auth.js");
 
 router.get("/all/:id", isAuth, getAllUsers);
 router.get("/:id", isAuth, getUser);
-router.patch("/edit/:id", isAuth, imageProcess, editUserValidator, editUser);
+router.patch("/edit/:id", isAuth, editUserValidator, editUser);
 
 module.exports = router;
