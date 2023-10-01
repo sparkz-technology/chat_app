351516ceb4aaa582ef4d555130712ee9b007f275
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-01 19:20:46 +0530

    url_changed

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -14,7 +14,9 @@ export default function Chat() {
   const currentUserId = localStorage.getItem("userId");
   useEffect(() => {
     if (currentUserId) {
-      socket.current = io(API_URL);
+      socket.current = io("wss://chat-app-dkot.vercel.app/", {
+        transports: ["websocket"],
+      });
       if (socket.current) {
         socket.current.on("connect", () => {
           socket.current.emit("add-user", currentUserId);
