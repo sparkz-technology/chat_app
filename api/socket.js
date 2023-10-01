cb6fa8c9ece8ad4a8da45f27693d10b98c31536f
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-01 17:01:34 +0530

    all-access

--- api/socket.js
+++ api/socket.js
@@ -9,7 +9,14 @@ const reconnectThreshold = 60000;
 
 // Function to set up the Socket.IO server
 const setupSocket = (server, origin) => {
-  const io = new Server(server);
+  const io = new Server(server,{
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "OPTIONS"],
+      credentials: true
+    }
+  });
+  }
 
   // Handle socket connections
   io.on("connection", (socket) => {
