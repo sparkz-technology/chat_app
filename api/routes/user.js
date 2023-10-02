ff90b7d59e03ef4c95365a69fe954e36adc787a5
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-10-02 16:24:00 +0530

    sa

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,16 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
-const { editUserValidator } = require("../validators/user.js");
-const { getAllUsers, editUser, getUser } = require("../controllers/user.js");
-const imageProcess = require("../utils/imageProcess.js");
-const isAuth = require("../middlewares/is-Auth.js");
+if (!fs.existsSync(path.join(__dirname, "images"))) {
+  fs.mkdirSync(path.join(__dirname, "images"));
+}
 
-router.use(multer().single("image"));
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "images"));
+  },
+  filename: function (req, file, cb) {
+    req.body.avatarImage = Date.now() + "-" + file.originalname;
+    cb(null, req.body.avatarImage);
+  },
+});
+
+const fileSizes = 1024 * 1024 * 10; // 10MB
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type."), false);
+  }
+};
+
+const uploadImage = multer({
+  storage: storage,
+  limits: { fileSize: fileSizes },
+  fileFilter: fileFilter,
+}).single("image");
+
+const { editUserValidator } = require("./validators/user.js");
+const { getAllUsers, editUser, getUser } = require("./controllers/user.js");
+const isAuth = require("./middlewares/is-Auth.js");
+
+router.use(uploadImage);
 
 router.get("/all/:id", isAuth, getAllUsers);
 router.get("/:id", isAuth, getUser);
-router.patch("/edit/:id", isAuth, editUserValidator, imageProcess, editUser);
+router.patch("/edit/:id", isAuth, editUserValidator, editUser);
 
 module.exports = router;
